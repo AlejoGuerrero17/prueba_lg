009002ab46e69618e1601624ebd760a6bd1a6866
Alejandro Guerrero Gómez <124387776+AlejoGuerrero17@users.noreply.github.com>
2023-06-23 11:31:12 -0500

    Agrega programa que calcula lista de multiplos de un numero
    Ademas se coloca encabezado a cada programa

--- list_numbers.py
+++ list_numbers.py
@@ -1,3 +1,4 @@
+#First program [Calculate numbers cousins from a list given]
 input('Ingrese lista de números separados por coma: ')
 numbers = numbers.replace(" ","")
 if len(my_numbers) >0:
@@ -5,4 +6,7 @@ if len(my_numbers) >0:
   else:
         pares = [numero for numero in my_numbers if float(numero) %2 == 0]
 print (f'Lista de número pares {pares}')
-
+#Second program [Calculate divisor from a given number]
+numero = int(input('Ingrese un número para conocer sus divisores: '))
+divisores = [divisor for divisor in range(1,numero+1) if numero % divisor == 0]
+print(divisores)
